e8ecc28a35084814a3094f4800d147035290b680
Aminur12-tech <alexdyel1234@gmail.com>
2025-10-14 13:29:24 +0530

    minor

--- controllers/root.js
+++ controllers/root.js
@@ -1,6 +1,7 @@
 const Listing = require("../models/listing");
 
+
 module.exports.root = async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("/index.ejs", { allListings });
+  res.render("index.js", { allListings });
 };
