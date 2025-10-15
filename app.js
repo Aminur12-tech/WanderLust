a2219e805cf130d9c9a59cdb4c09d4425c73a803
Aminur12-tech <alexdyel1234@gmail.com>
2025-10-15 09:11:36 +0530

    minor

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('i am root');  // renders views/index.ejs
+  res.render('index.ejs');  // renders views/index.ejs
 });
 
 
